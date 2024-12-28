138c71ec2c1acd92da25251046dced9ea4ab44d8
WJF <1958938125@qq.com>
2024-12-28 11:05:23 +0800

    12.28

--- src/DS1302/seg.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SEG_H
-#define _SEG_H
-
-void Seg(unsigned char *seg_num ,unsigned char length);
-void Seg_Disp(unsigned char* seg_buf);
-#endif
--- src/DS18B20/DS18B20.h
+++
@@ -1,8 +0,0 @@
-#ifndef  _DS18B20_H
-#define  _DS18B20_H
-
-unsigned char DS18B20Init();
-void DS18B20WriteByte(unsigned char Datebyte);
-unsigned char DS18B20ReadByte();
-
-#endif // ! _DS18B20_H
